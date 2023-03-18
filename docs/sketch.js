68c4bbc9abfebbc179ee726035fb14d846077414
smortty <126961511+smortty@users.noreply.github.com>
2023-03-18 10:52:17 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,50 +1,100 @@
+let lambo, ferrari;
+let lamboX, ferrariX;
+let lamboY, ferrariY;
+let speed = 4;
+let jumping = false;
+let jumpCounter = 0;
 
-// Where is the car
-let x, y;
-class Carro{
-  constructor(alto,ancho,color,name){
+class Carro {
+  constructor(alto, ancho, color, name) {
     this.alto = alto;
     this.ancho = ancho;
     this.color = color;
     this.name = name;
   }
+
+  display(x, y) {
+    stroke(220, 200, 200);
+    fill(this.color);
+    rect(x, y, this.ancho, this.alto);
+  }
 }
+
 function setup() {
-  createCanvas(700, 500);
-  // Start position
-  y = height - height/4;
-  x = 0;
-  // crear una instancia de Carro llamada Lambo
-  let lambo = new Carro (10,20,blue,"lambo")
+  createCanvas(600, 500);
+  lamboX = 0;
+  ferrariX = width;
+  lamboY = height - height / 4;
+  ferrariY = height - height / 4;
+
+  lambo = new Carro(30, 60, "blue", "lambo");
+  ferrari = new Carro(30, 60, "green", "ferrari");
+
   console.log("lambo ancho es " + lambo.ancho);
   console.log("lambo alto es " + lambo.alto);
   console.log("lambo color es " + lambo.color);
   console.log("lambo name es " + lambo.name);
+
+  console.log("ferrari ancho es " + ferrari.ancho);
+  console.log("ferrari alto es " + ferrari.alto);
+  console.log("ferrari color es " + ferrari.color);
+  console.log("ferrari name es " + ferrari.name);
+} if (ferrariX > width - ferrari.ancho) {
+    ferrariX = width - ferrari.ancho;
+    speed *= -1;
+} else if (ferrariX < ferrari.ancho) {
+    ferrariX = ferrari.ancho;
+    speed *= -1;
 }
 
-
 function draw() {
-  background(20,200,150);  
-  display();
-  move();
-}  
-function move(){
-  // Jiggling randomly
-  y = y + random(-2, 2);
-  
-  if (x > width) {
-    // Reset 
-    x = 0;
-    y = height - height/4;
+  background(20, 200, 150);
+
+  lamboX += speed;
+  ferrariX -= speed;
+
+  if (lamboX > width - lambo.ancho) {
+    lamboX = width - lambo.ancho;
+    speed * -1;
+  } else if (lamboX < 0) {
+    lamboX = 0;
+    speed * -1;
+  }
+
+  if (ferrariX < ferrari.ancho) {
+    ferrariX = ferrari.ancho;
+    speed *= -1;
+  } else if (ferrariX > width) {
+    ferrariX = width;
+    speed *= -1;
+  }
+
+  if (jumping) {
+    if (jumpCounter < 10) {
+      lamboY -= 5;
+    } else if (jumpCounter < 20) {
+      lamboY += 5;
+    } else {
+      jumping = false;
+    }
+    jumpCounter++;
   } else {
-    // Moving up at a constant speed
-    x = x + 2;
+    lamboY = height - height / 4;
+  }
+
+  lambo.display(lamboX, lamboY);
+  ferrari.display(ferrariX, ferrariY);
+}
+
+function salto() {
+  if (!jumping) {
+    jumping = true;
+    jumpCounter = 0;
   }
 }
 
-function display(){
-  // Draw a car
-  stroke(220,200,200);
-  fill(220,80,200);
-  rect(x, y, 50, 30);
+function keyPressed() {
+  if (keyCode === 32) {
+    salto();
+  }
 }
